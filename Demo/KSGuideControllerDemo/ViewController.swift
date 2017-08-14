d67b7fabef24e9e2d7cbee3ac1b1a09ec2ff6455
Kyle Sun <skx926@gmail.com>
2017-08-14 11:09:41 +0800

    Update the demo project.

--- Demo/KSGuideControllerDemo/ViewController.swift
+++ Demo/KSGuideControllerDemo/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
-    let string = "后来我转行干了段时间销售，赚的比之前多了点，好的时候一个月能拿两三万。我看我妈的手机又旧又破，接电话的时候还要扯着嗓子喊。于是给她买了当时最新款的水果手机，我把手机盒递给她的时候她又炸毛了，又嚷嚷着说太贵了要去退。这次我学聪明了，说发票已经掉了退不了，我妈彻底毛了，好几天不给我做饭。"
+    let string = "GitHub is a web-based Git or version control repository and Internet hosting service. It is mostly used for code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project."
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,8 +45,11 @@ class ViewController: UIViewController {
             items.append(item)
         }
         let vc = KSGuideController(items: items, key: "MainGuide")
-        vc.setIndexChangeBlock { (index, item) in
-            print("Index has change to \(index)")
+        vc.setIndexWillChangeBlock { (index, item) in
+            print("Index will change to \(index)")
+        }
+        vc.setIndexDidChangeBlock { (index, item) in
+            print("Index did change to \(index)")
         }
         vc.show(from: self) { 
             print("Guide controller has been dismissed")
