2f8ec62e32bb06800f7fe9ffc413843ea656e289
CocoaBob <mengke.wang@gmail.com>
2017-11-17 14:31:15 -0500

    Support Obj-C projects.

--- KSGuideController/KSGuideDataManager.swift
+++ KSGuideController/KSGuideDataManager.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 
-public class KSGuideDataManager: NSObject {
+@objc public class KSGuideDataManager: NSObject {
     
     static let userDefaults = UserDefaults.standard
     static let dataKey = "KSGuideDataKey"
     
-    public static func reset(for key: String) {
+    @objc public static func reset(for key: String) {
         if var data = userDefaults.object(forKey: dataKey) as? [String: Bool] {
             data.removeValue(forKey: key)
             userDefaults.set(data, forKey: dataKey)
         }
     }
     
-    public static func resetAll() {
+    @objc public static func resetAll() {
         userDefaults.set(nil, forKey: dataKey)
     }
     
